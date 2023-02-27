a021e0d821020538d3f1e2aeb752272cf4973b55
Kurtis Knodel <kurtis@kurtisknodel.com>
2023-02-27 14:44:37 -0800

    Removed use of setx

--- install.bat
+++ install.bat
@@ -3,6 +3,5 @@
 cargo build --release
 mkdir %LOCALAPPDATA%\.codesearch\
 copy target\release\codesearch.exe %LOCALAPPDATA%\.codesearch\
-setx PATH "%PATH%;%LOCALAPPDATA%\.codesearch\"
 
-echo Built codesearch, moved it to %LOCALAPPDATA%\.codesearch\ and added it to PATH.
+echo Setup complete. To use codesearch from the command line, add %LOCALAPPDATA%\.codesearch to the system PATH.
